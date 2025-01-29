25c841b86cf026932a8c5c553467127ac6169ff2
jameskurlander-fortune <james.kurlander@fortune.com>
2025-01-29 14:04:14 -0500

    Renamed plugin files and fixed lsp issues.

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,24 +3,30 @@ return {
   config = function()
     local lspconfig = require("lspconfig");
 
+    --bash
+    lspconfig.bashls.setup({});
+    -- lspconfig.shfmt.setup({});
+    -- lspconfig.shellcheck.setup({});
+
+    --javascript, typescript, jsx, tsx, json, css, graphql, html
+    lspconfig.biome.setup({});
     lspconfig.cssls.setup({});
-    lspconfig.somesass_ls.setup({});
     lspconfig.graphql.setup({});
     lspconfig.html.setup({});
-    lspconfig.lua_ls.setup({});
-    lspconfig.ts_ls.setup({});
-    lspconfig.eslint.setup({});
-    lspconfig.phpactor.setup({
-        filetypes = { 'php', 'blade' },
-      });
     lspconfig.tailwindcss.setup({});
+    lspconfig.ts_ls.setup({});
 
-    lspconfig.typos_lsp.setup({
-        cmd_env = { RUST_LOG = "error" },
-        init_options = {
-          config = "~/code/typos-lsp/crates/typos-lsp/tests/typos.toml",
-          diagnosticSeverity = "Hint",
-        },
-      });
+    --lua
+    lspconfig.lua_ls.setup({});
+    -- lspconfig.luacheck.setup({});
+    -- lspconfig.stylua.setup({});
+
+    --php
+    -- lspconfig.blade_formatter.setup({});
+    lspconfig.intelephense.setup({});
+    lspconfig.phpactor.setup({ filetypes = { 'php', 'blade' } });
+
+    --other
+    lspconfig.typos_lsp.setup({});
   end,
 }
