8bbf07258f5d9c48ff72b34e24cb7a0a13641bf2
JamesKurlander <james.h.kurlander@gmail.com>
2025-02-01 17:47:03 -0500

    Fixed tmux windows appearance.

---
+++ scripts/spotify_duration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+playback_info=$(spotify_player get key playback)
+
+if [ -n "$playback_info" ]; then
+  progress_ms=$(echo "$playback_info" | jq -r '.progress_ms')
+  progress_min=$((progress_ms / 60000))
+  progress_sec=$(( (progress_ms / 1000) % 60))
+
+  duration_ms=$(echo "$playback_info" | jq -r '.item.duration_ms')
+  duration_min=$((duration_ms / 60000))
+  duration_sec=$(( (duration_ms / 1000) % 60))
+
+  progress_formatted=$(printf "%d:%02d" $progress_min $progress_sec)
+  duration_formatted=$(printf "%d:%02d" $duration_min $duration_sec)
+
+  echo "[$progress_formatted / $duration_formatted]"
+fi
--- scripts/spotify_playback.sh
+++ scripts/spotify_playback.sh
@@ -1,48 +1,34 @@
 #!/bin/bash
 
 playback_info=$(spotify_player get key playback)
-status_line=""
 
 if [ -n "$playback_info" ]; then
-  artist_name=$(echo "$playback_info" | jq -r '.item.artists[0].name')
-  song_name=$(echo "$playback_info" | jq -r '.item.name')
-
-  if [ "$song_name" = "null" ]; then
-    exit 0
-  fi
-
-  is_playing=$(echo "$playback_info" | jq -r '.is_playing')
-  shuffle_state=$(echo "$playback_info" | jq -r '.shuffle_state')
-  repeat_state=$(echo "$playback_info" | jq -r '.repeat_state')
-
-  progress_ms=$(echo "$playback_info" | jq -r '.progress_ms')
-  duration_ms=$(echo "$playback_info" | jq -r '.item.duration_ms')
-
-  progress_min=$((progress_ms / 60000))
-  progress_sec=$(( (progress_ms / 1000) % 60))
-  duration_min=$((duration_ms / 60000))
-  duration_sec=$(( (duration_ms / 1000) % 60))
-
-  progress_formatted=$(printf "%d:%02d" $progress_min $progress_sec)
-  duration_formatted=$(printf "%d:%02d" $duration_min $duration_sec)
-
-  # if [ "$repeat_state" = "track" ]; then
-  #   status_line+="↺T "
-  # elif [ "$repeat_state" = "context" ]; then
-  #   status_line+="↺C "
-  # fi
+  symbol=''
+  shuffle_state=$(echo "$playback_info" |
+    jq -r '.shuffle_state')
+  artist=$(echo "$playback_info" |
+    jq -r '.item.artists[0].name')
+  album=$(echo "$playback_info" |
+    jq -r '.item.album.name')
+  year=$(echo "$playback_info" |
+    jq -r '.item.album.release_date' |
+    cut -d '-' -f 1)
+  song=$(echo "$playback_info" |
+    jq -r '.item.name')
+  is_playing=$(echo "$playback_info" |
+    jq -r '.is_playing')
+  repeat_state=$(echo "$playback_info" |
+    jq -r '.repeat_state')
 
   if [ "$is_playing" = "true" ]; then
     if [ "$shuffle_state" = true ]; then
-      status_line+="▶ "
+      symbol='▶'
     else
-      status_line+="▷ "
+      symbol='▷'
     fi
   else
-    status_line+="⏸ "
+    symbol='■'
   fi
 
-  status_line+="$artist_name - $song_name [$progress_formatted/$duration_formatted]"
-
-  echo "$status_line"
+  echo "$symbol $artist - $album ($year) - $song"
 fi
