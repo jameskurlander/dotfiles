7783cb57f2516bc9d78159def9782e1c4050d5de
JamesKurlander <james.h.kurlander@gmail.com>
2025-02-02 16:50:38 -0500

    Added clock to tmux statusline.

--- nvim/lua/plugins/toggleterm.nvim.lua
+++ nvim/lua/plugins/toggleterm.nvim.lua
@@ -6,8 +6,6 @@ return {
       size = 20,
       open_mapping = [[<c-\>]],
       hide_numbers = true,
-      shade_filetypes = {},
-      shade_terminals = true,
       start_in_insert = true,
       insert_mappings = true,
       persist_size = true,
