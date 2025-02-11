dcfa60d7a8e8b270dbce9f276928c8cd6381fa7d
jameskurlander-fortune <james.kurlander@fortune.com>
2025-02-11 15:50:01 -0500

    Fixed merge issues.

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,71 +1,70 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require("lspconfig");
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
 
-    --bash
-    lspconfig.bashls.setup({});
-    -- lspconfig.shfmt.setup({});
-    -- lspconfig.shellcheck.setup({});
+		--bash
+		lspconfig.bashls.setup({})
+		-- lspconfig.shfmt.setup({});
+		-- lspconfig.shellcheck.setup({});
 
-    --javascript, typescript, jsx, tsx, json, css, graphql, html
-    lspconfig.biome.setup({
-      cmd = { 'biome', 'lsp-proxy' },
-      filetypes = {
-        'astro',
-        'css',
-        'graphql',
-        'javascript',
-        'javascriptreact',
-        'json',
-        'jsonc',
-        'svelte',
-        'typescript',
-        'typescript.tsx',
-        'typescriptreact',
-        'vue',
-      },
-      root_dir = lspconfig.util.root_pattern('biome.json', 'biome.jsonc'),
-      single_file_support = false,
-    });
-    lspconfig.cssls.setup({});
-    lspconfig.graphql.setup({});
-    lspconfig.html.setup({});
-    lspconfig.tailwindcss.setup({
-      settings = {
-        tailwindCSS = {
-          experimental = {
-            classRegex = {
-              { "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" }
-            },
-          },
-        },
-      },
-    })
-    });
-    lspconfig.ts_ls.setup({});
+		--javascript, typescript, jsx, tsx, json, css, graphql, html
+		lspconfig.biome.setup({
+			cmd = { "biome", "lsp-proxy" },
+			filetypes = {
+				"astro",
+				"css",
+				"graphql",
+				"javascript",
+				"javascriptreact",
+				"json",
+				"jsonc",
+				"svelte",
+				"typescript",
+				"typescript.tsx",
+				"typescriptreact",
+				"vue",
+			},
+			root_dir = lspconfig.util.root_pattern("biome.json", "biome.jsonc"),
+			single_file_support = false,
+		})
+		lspconfig.cssls.setup({})
+		lspconfig.graphql.setup({})
+		lspconfig.html.setup({})
+		lspconfig.tailwindcss.setup({
+			settings = {
+				tailwindCSS = {
+					experimental = {
+						classRegex = {
+							{ "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" },
+						},
+					},
+				},
+			},
+		})
+		lspconfig.ts_ls.setup({})
 
-    --lua
-    lspconfig.lua_ls.setup({
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-        },
-      },
-    });
-    -- lspconfig.luacheck.setup({});
-    -- lspconfig.stylua.setup({});
+		--lua
+		lspconfig.lua_ls.setup({
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		})
+		-- lspconfig.luacheck.setup({});
+		-- lspconfig.stylua.setup({});
 
-    --php
-    -- lspconfig.blade_formatter.setup({});
-    lspconfig.intelephense.setup({});
-    lspconfig.phpactor.setup({
-      filetypes = { 'php', 'blade' }
-    });
+		--php
+		-- lspconfig.blade_formatter.setup({});
+		lspconfig.intelephense.setup({})
+		lspconfig.phpactor.setup({
+			filetypes = { "php", "blade" },
+		})
 
-    --other
-    lspconfig.typos_lsp.setup({});
-  end,
+		--other
+		lspconfig.typos_lsp.setup({})
+	end,
 }
