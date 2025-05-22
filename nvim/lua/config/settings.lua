55c08a428c5367d8950643178a95204b97e1bc78
jameskurlander-fortune <james.kurlander@fortune.com>
2025-05-22 16:16:27 -0400

    Added ghostty-theme-sync plugin to sync theme with Neovim.

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -1,5 +1,6 @@
 -- theme (must be at top to maintain highlight overrides)
-vim.cmd([[colorscheme moonfly]])
+-- vim.cmd.colorscheme("moonfly")
+vim.cmd.colorscheme("tokyonight-night")
 vim.opt.termguicolors = true
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
---
+++ nvim/lua/plugins/ghostty-theme-sync.lua
@@ -0,0 +1,9 @@
+return {
+	"landerson02/ghostty-theme-sync.nvim",
+	--- @type GhosttySyncConfig
+	opts = {
+		ghostty_config_path = "~/.config/ghostty/config",
+		persist_nvim_theme = true,
+		nvim_config_path = "~/.config/nvim/lua/config/settings.lua",
+	},
+}
