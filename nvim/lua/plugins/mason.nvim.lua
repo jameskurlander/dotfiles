82ae928aa954ce4fc8fbbdb1fd1c0e1e333bd5ff
JamesKurlander <james.h.kurlander@gmail.com>
2025-05-25 21:41:25 -0400

    Added LSP for C and C++.

--- nvim/lua/plugins/mason.nvim.lua
+++ nvim/lua/plugins/mason.nvim.lua
@@ -1,51 +1,54 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		require("mason").setup({
-			ui = {
-				border = "rounded",
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    require("mason").setup({
+      ui = {
+        border = "rounded",
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		require("mason-lspconfig").setup({
-			automatic_installation = true,
-			ensure_installed = {
-				--bash
-				"bashls",
-				-- 'shfmt',
-				-- 'shellcheck',
+    require("mason-lspconfig").setup({
+      automatic_installation = true,
+      ensure_installed = {
+        --bash
+        "bashls",
+        -- 'shfmt',
+        -- 'shellcheck',
 
-				--javascript, typescript, jsx, tsx, json, css, graphql, html
-				"biome",
-				"cssls",
-				"graphql",
-				"html",
-				"tailwindcss",
-				"ts_ls",
+        -- c, c++
+        "clangd",
 
-				--lua
-				"lua_ls",
-				-- 'luacheck',
-				-- 'stylua',
+        --javascript, typescript, jsx, tsx, json, css, graphql, html
+        "biome",
+        "cssls",
+        "graphql",
+        "html",
+        "tailwindcss",
+        "ts_ls",
 
-				--php
-				-- 'blade-formatter',
-				"intelephense",
-				"phpactor",
+        --lua
+        "lua_ls",
+        -- 'luacheck',
+        -- 'stylua',
 
-				--other
-				"typos_lsp",
-			},
-		})
-	end,
+        --php
+        -- 'blade-formatter',
+        "intelephense",
+        "phpactor",
+
+        --other
+        "typos_lsp",
+      },
+    })
+  end,
 }
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,71 +1,74 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require("lspconfig")
 
-		--bash
-		vim.lsp.enable("bashls")
-		vim.lsp.enable("shfmt")
-		vim.lsp.enable("shellcheck")
+    --bash
+    vim.lsp.enable("bashls")
+    vim.lsp.enable("shfmt")
+    vim.lsp.enable("shellcheck")
 
-		--javascript, typescript, jsx, tsx, json, css, graphql, html
-		vim.lsp.enable("biome", {
-			cmd = { "biome", "lsp-proxy" },
-			filetypes = {
-				"astro",
-				"css",
-				"graphql",
-				"javascript",
-				"javascriptreact",
-				"json",
-				"jsonc",
-				"svelte",
-				"typescript",
-				"typescript.tsx",
-				"typescriptreact",
-				"vue",
-			},
-			root_dir = lspconfig.util.root_pattern("biome.json", "biome.jsonc"),
-			single_file_support = false,
-		})
+    -- c, c++
+    vim.lsp.enable("clangd")
 
-		vim.lsp.enable("cssls")
-		vim.lsp.enable("graphql")
-		vim.lsp.enable("html")
-		vim.lsp.enable("tailwindcss", {
-			settings = {
-				tailwindCSS = {
-					experimental = {
-						classRegex = {
-							{ "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" },
-						},
-					},
-				},
-			},
-		})
-		vim.lsp.enable("ts_ls")
+    --javascript, typescript, jsx, tsx, json, css, graphql, html
+    vim.lsp.enable("biome", {
+      cmd = { "biome", "lsp-proxy" },
+      filetypes = {
+        "astro",
+        "css",
+        "graphql",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "jsonc",
+        "svelte",
+        "typescript",
+        "typescript.tsx",
+        "typescriptreact",
+        "vue",
+      },
+      root_dir = lspconfig.util.root_pattern("biome.json", "biome.jsonc"),
+      single_file_support = false,
+    })
 
-		--lua
-		vim.lsp.enable("lua_ls", {
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		})
-		-- vim.lsp.enable("luacheck")
-		-- vim.lsp.enable("stylua")
+    vim.lsp.enable("cssls")
+    vim.lsp.enable("graphql")
+    vim.lsp.enable("html")
+    vim.lsp.enable("tailwindcss", {
+      settings = {
+        tailwindCSS = {
+          experimental = {
+            classRegex = {
+              { "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" },
+            },
+          },
+        },
+      },
+    })
+    vim.lsp.enable("ts_ls")
 
-		--php
-		-- vim.lsp.enable("blade_formatter");
-		vim.lsp.enable("intelephense")
-		vim.lsp.enable("phpactor", {
-			filetypes = { "php", "blade" },
-		})
+    --lua
+    vim.lsp.enable("lua_ls", {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
+    -- vim.lsp.enable("luacheck")
+    -- vim.lsp.enable("stylua")
 
-		--other
-		vim.lsp.enable("typos_lsp")
-	end,
+    --php
+    -- vim.lsp.enable("blade_formatter");
+    vim.lsp.enable("intelephense")
+    vim.lsp.enable("phpactor", {
+      filetypes = { "php", "blade" },
+    })
+
+    --other
+    vim.lsp.enable("typos_lsp")
+  end,
 }
