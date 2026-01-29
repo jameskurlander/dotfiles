f6ab7aa3e2c2c0bc52d314e565e4c7e28a7f2e1a
James Kurlander <163871657+jameskurlander-fortune@users.noreply.github.com>
2026-01-29 13:28:01 -0500

    Made a bunch of updates to setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 DOTFILES_DIR="$HOME/Repos/dotfiles"
+ORANGE=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+RESET=$(tput sgr0)
 
 personal_flag=false
 
@@ -12,15 +16,17 @@ while getopts "p" flag; do
   esac
 done
 
+# Set MacOS defaults
 echo "Setting MacOS defaults..."
 sh $DOTFILES_DIR/osx/set-defaults.sh
 
-# Install Homebrew
+# Install & Update Homebrew
 if ! command -v brew &>/dev/null; then
-  echo "Homebrew is not installed. Installing Homebrew..."
+  echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-  echo "Homebrew is already installed."
+  echo "${GREEN}Updating Homebrew...${RESET}"
+  brew update
 fi
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -28,57 +34,89 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # Install Homebrew packages
 brew bundle --file="$DOTFILES_DIR/homebrew/Brewfile.work"
 
-if [[ "$personal_flag" == true && -f "$DOTFILES_DIR/homebrew/Brewfile.personal" ]]; then
-  echo "Installing from Brewfile.personal..."
-  brew bundle --file="$DOTFILES_DIR/homebrew/Brewfile.personal"
-else
-  echo "Brewfile.personal not found in dotfiles directory."
+if [[ "$personal_flag" == true ]]; then
+  if [[ -f "$DOTFILES_DIR/homebrew/Brewfile.personal" ]]; then
+    echo "Installing from Brewfile.personal..."
+    brew bundle --file="$DOTFILES_DIR/homebrew/Brewfile.personal"
+  else
+    echo "${ORANGE}WARNING: Brewfile.personal not found in dotfiles directory.${RESET}"
+  fi
 fi
 
-# Symlink for opencod 
-rm -f "$HOME/.config/opencode/opencode.jsonc"
-ln -s "$DOTFILES_DIR/opencode/opencode.jsonc" "$HOME/.config/opencode/opencode.jsonc"
-echo "Symlinked opencode config"
-
-# Symlink for ghostty
-rm -rf "$HOME/.config/ghostty"
-ln -sf "$DOTFILES_DIR/ghostty" "$HOME/.config/ghostty"
-echo "Symlinked ghostty config"
+# Update Homebrew packages
+echo "${GREEN}Updating Homebrew packages...${RESET}"
+brew upgrade
 
 # Symlink for zsh
 rm -rf "$HOME/.zshrc"
 ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
-echo "Symlinked zsh config"
+echo "${BLUE}Symlinked zsh config${RESET}"
 
 # Symlink for zsh_plugins
 rm -rf "$HOME/.zsh_plugins.txt"
 ln -sf "$DOTFILES_DIR/zsh/zsh_plugins.txt" "$HOME/.zsh_plugins.txt"
-echo "Symlinked zsh plugins"
+echo "${BLUE}Symlinked zsh plugins${RESET}"
 
-# Symlink for neovim
-rm -rf "$HOME/.config/nvim"
-ln -sf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
-echo "Symlinked neovim config"
+# Update zsh plugins
+if command -v antidote &>/dev/null; then
+  echo "${GREEN}Updating zsh plugins...${RESET}"
+  antidote update
+else
+  echo "${ORANGE}WARNING: antidote not installed. Skipping zsh plugin update.${RESET}"
+fi
 
 # Symlink for tmux
 rm -rf "$HOME/.tmux.conf"
 ln -sf "$DOTFILES_DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
-echo "Symlinked tmux config"
+echo "${BLUE}Symlinked tmux config${RESET}"
+
+# Update tmux plugins
+if command -v tmux &>/dev/null; then
+  if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    echo "${ORANGE}WARNING: tpm not installed. Skipping tmux plugin update.${RESET}"
+  else
+    echo "${GREEN}Updating tmux plugins...${RESET}"
+    if [ -z "$TMUX" ]; then
+      tmux start-server
+      ~/.tmux/plugins/tpm/bin/update_plugins all
+      tmux kill-server
+    else
+      ~/.tmux/plugins/tpm/bin/update_plugins all
+    fi
+  fi
+else
+  echo "${ORANGE}WARNING: tmux not installed. Skipping tmux plugin update.${RESET}"
+fi
+
+# Symlink for opencode
+rm -f "$HOME/.config/opencode/opencode.jsonc"
+ln -s "$DOTFILES_DIR/opencode/opencode.jsonc" "$HOME/.config/opencode/opencode.jsonc"
+echo "${BLUE}Symlinked opencode config${RESET}"
+
+# Symlink for ghostty
+rm -rf "$HOME/.config/ghostty"
+ln -sf "$DOTFILES_DIR/ghostty" "$HOME/.config/ghostty"
+echo "${BLUE}Symlinked ghostty config${RESET}"
+
+# Symlink for neovim
+rm -rf "$HOME/.config/nvim"
+ln -sf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
+echo "${BLUE}Symlinked neovim config${RESET}"
 
 # Symlink for spotify-player
 rm -rf "$HOME/.config/spotify-player"
 ln -sf "$DOTFILES_DIR/spotify-player" "$HOME/.config/spotify-player"
-echo "Symlinked spotify-player config"
+echo "${BLUE}Symlinked spotify-player config${RESET}"
 
 # Symlink for yazi
 rm -rf "$HOME/.config/yazi"
 ln -sf "$DOTFILES_DIR/yazi" "$HOME/.config/yazi"
-echo "Symlinked yazi config"
+echo "${BLUE}Symlinked yazi config${RESET}"
 
 # Symlink for newsboat
 rm -rf "$HOME/.newsboat"
 ln -sf "$DOTFILES_DIR/newsboat" "$HOME/.newsboat"
-echo "Symlinked newsboat config"
+echo "${BLUE}Symlinked newsboat config${RESET}"
 
 # Refresh the terminal
 reset
