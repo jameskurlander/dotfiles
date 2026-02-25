c34eadaa236dd6e1bc1968b3ea18f4fa5e4c40e1
James Kurlander <163871657+jameskurlander-fortune@users.noreply.github.com>
2026-02-25 13:54:52 -0500

    Removed unnecessary code from opencode config.

--- nvim/lua/plugins/opencode.nvim.lua
+++ nvim/lua/plugins/opencode.nvim.lua
@@ -1,14 +1,3 @@
 return {
   "NickvanDyke/opencode.nvim",
-  config = function()
-    ---@type opencode.Opts
-    vim.g.opencode_opts = {
-      provider = {
-        enabled = "tmux",
-      },
-    }
-
-    -- Required for `opts.events.reload`.
-    vim.o.autoread = true
-  end,
 }
