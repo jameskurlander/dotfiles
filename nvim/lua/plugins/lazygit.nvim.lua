718129172389b89fdfd87d6005dc74443571bc5d
James Kurlander <163871657+jameskurlander-fortune@users.noreply.github.com>
2026-07-07 16:03:30 -0400

    Make LazyGit popup fullscreen and borderless

--- nvim/lua/plugins/lazygit.nvim.lua
+++ nvim/lua/plugins/lazygit.nvim.lua
@@ -3,6 +3,10 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
+	init = function()
+		vim.g.lazygit_floating_window_scaling_factor = 1.0
+		vim.g.lazygit_floating_window_border_chars = { "", "", "", "", "", "", "", "" }
+	end,
 	cmd = {
 		"LazyGit",
 		"LazyGitConfig",
